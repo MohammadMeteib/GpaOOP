aa89d02f59fa36f5bcdf8bce7eb73e016ff87f04
ABK <ABK@DESKTOP-PTCS16K>
2024-12-24 14:45:40 +0300

    Add project files.

---
+++ Gpa/Models/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gpa.Models
+{
+    public class Student
+    {
+        public string StudentInfo(string name, string major, int gpa)
+        {
+            return "Student name is: " + name + ". Major is: " + major + ". GPA is: " + gpa;
+        }
+    }
+}
---
+++ Gpa/Program.cs
@@ -0,0 +1,14 @@
+﻿using Gpa.Models;
+Student student = new Student();
+string name;
+string major;
+int gpa;
+
+Console.WriteLine("Enter name");
+name = Console.ReadLine();
+Console.WriteLine("Enter major");
+major = Console.ReadLine();
+Console.WriteLine("Enter GPA");
+gpa = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(student.StudentInfo(name, major, gpa));
